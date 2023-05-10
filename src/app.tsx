007edeb37ee89c388b71fa9ad676a85a1865f532
Permadi Wibisono <mbapewe@gmail.com>
2023-05-10 11:25:15 +0700

    add ThemeProvider in app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,41 @@
-import { Suspense, lazy } from "react";
+import { ThemeProvider } from "@emotion/react";
+import { Suspense, lazy, useState } from "react";
+import * as themes from "./themes";
 
 const Calculator = lazy(() => import("@containers/calculator"));
 
 function App() {
+  const [theme, setTheme] = useState("dark");
   return (
-    <div>
+    <ThemeProvider theme={theme === "dark" ? themes.dark : themes.light}>
       <Suspense fallback={null}>
         <Calculator />
       </Suspense>
+      <div style={{ marginTop: 30 }}>
+        <fieldset>
+          <legend>Theme</legend>
+          <label>
+            <input
+              onChange={() => setTheme("light")}
+              checked={theme === "light"}
+              type="radio"
+              name="theme"
+              value="light"
+            />{" "}
+            light
+          </label>
+          <label>
+            <input
+              onChange={() => setTheme("dark")}
+              checked={theme === "dark"}
+              type="radio"
+              name="theme"
+              value="dark"
+            />{" "}
+            dark
+          </label>
+        </fieldset>
+      </div>
       <div style={{ marginTop: 30, textAlign: "center" }}>
         Calculator component <a href="https://codepen.io/mjijackson/pen/xOzyGX">created</a>
         {" by "}
@@ -15,7 +43,7 @@ function App() {
         <a href="https://twitter.com/mjackson">Michael Jackson</a> of{" "}
         <a href="https://reacttraining.com/">React Training</a>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
